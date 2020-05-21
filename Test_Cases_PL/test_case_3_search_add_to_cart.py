43ced7ea76fad92544b914b837970724fb5bde73
korzeniowska18 <57856955+korzeniowska18@users.noreply.github.com>
2020-05-21 08:44:32 +0200

    create case

---
+++ Test_Cases_PL/test_case_3_search_add_to_cart.py
@@ -0,0 +1,89 @@
+I. Przypadek testowy 3. Wyszukiwanie produktu i dodanie do koszyka
+
+ID: 003
+
+Tytuł: Wyszukiwanie produktu na stronie internetowej sklepu za pomocą filtrów i dodanie do koszyka
+
+Środowisko: Robot Framework version 3.1.2, Selenium Library version:	4.4.0, Chrome version 80.0.3987.163, PyCharm 2019.3.3
+
+Warunek wstępny: Uruchomiona przeglądarka. Użytkownik nie jest zalogowany
+
+
+Kroki:
+
+ Scenariusz 1: Wyszukiwanie produktu na stronie internetowej sklepu za pomocą filtrów
+ 
+1. Wejdź na stronę „https://centrumamis.pl/pl”
+
+2. Kliknij w polu „SZUKAJ”
+
+3. Wprowadź słowo kluczowe  „odchudzanie”
+
+4. Wybierz za pomoca filtra kategorię "Kontrola wagi"
+
+5. Wybierz za pomoca filtra producenta "Colfarm"
+
+6. Wprowadź w pole wyboru ceny za pomocą filtra najmniejszą wartość cenową "20"
+
+7. Wprowadź w pole wyboru ceny za pomocą filtra największą wartość cenową "50"
+
+8. Kliknij "FILTRUJ"
+
+9. Wypisz w konsoli liczbę odnalezionych produktów podając nazwy i ceny
+
+
+Wynik:
+
+Filtr w wyszukiwarce produktów pozwala na wybór kategorii, producenta i zakresu cen, oraz na tej podstawie odnajduje odpowiednie produkty
+
+Wynik oczekiwany):
+
+Filtr w wyszukiwarce produktów pozwala na wybór kategorii, producenta i zakresu cen, oraz na tej podstawie odnajduje odpowiednie produkty
+
+Uwagi końcowe:
+
+Ten test ma wynik pozytywny. To znaczy, że filtr w wyszukiwarce działa poprawnie, czyli pozwala na wybór kategorii, producenta i zakresu cen, oraz na tej podstawie odnajduje odpowiednie produkty
+
+Additional information:
+
+In scenario 3 was confirmed that filter works correctly and assertion for category, producent names and selection lower/higher price work correctly. 
+
+Kroki:
+
+Scenariusz 2: Dodanie do koszyka pierwszego odnalezionego produktu i sprawdzenie czy pojawia się odpowiedni komunikat
+
+1. Wybierz pierwszy odnaleziony produkt
+2. Kliknij "DODAJ DO KOSZYKA"
+3. Pojawia się komukat "Produkt dodany do koszyka."
+4. Sprawdzamy czy komunikat jest odpowiedni
+
+Wynik:
+
+Dany test potwierdza, że po dodaniu produktu do koszyka pojawia się odpowiedni komunikat "Produkt dodany do koszyka."
+
+Wynik oczekiwany:
+
+Pojawienie się odpowiedniego komunikatu po dodaniu produktu do koszyka, który będzie informał, że produkt został dodany do koszyka. 
+
+
+Uwagi końcowe:
+
+Fukcja dodania produktu do koszyka działa poprawnie. Pojawia się odpowiedni komunikat po daniu produktu do koszyka. 
+
+Kroki:
+ Scenariusz 3. Prezprowadzenie asercji dotyczącej kategorii, nazwy i ceny produktu dodanego do koszyka
+
+1. Sprawdzenie nazwy kategorii, nazwy produktu i ceny dodanego do koszyka
+2. Porównywanie czy cena produktu w koszyku odpowiada cenie wybranej za pomocą filtra
+
+Wynik:
+
+Dany test potwierdza, że produkt dodany do koszyka zawiera odpowiednią nazwę kategorii, nazwe produktu i cenę według ustawień filtra.
+
+Wynik oczekiwany:
+
+Potwierdzić, że produkt dodany do koszyka zawiera odpowiednią nazwę kategorii, nazwe produktu i cenę według ustawień filtra.
+
+Uwagi końcowe:
+
+Filtr w wyszukiwarcę produktów umożliwiający wybranie kategorii, producenta i zakres cen działa poprawnie. 
